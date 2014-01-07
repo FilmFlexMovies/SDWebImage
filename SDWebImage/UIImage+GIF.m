b8a1719d1a5dd1e18362acedd6108081237ff8c3
Fabrice Aneche <akh@nobugware.com>
2014-01-06 17:12:24 -0800

    reformat code (appcode)

--- SDWebImage/UIImage+GIF.m
+++ SDWebImage/UIImage+GIF.m
@@ -11,31 +11,26 @@
 
 @implementation UIImage (GIF)
 
-+ (UIImage *)sd_animatedGIFWithData:(NSData *)data
-{
-    if (!data)
-    {
++ (UIImage *)sd_animatedGIFWithData:(NSData *)data {
+    if (!data) {
         return nil;
     }
-    
+
     CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)data, NULL);
-    
+
     size_t count = CGImageSourceGetCount(source);
 
     UIImage *animatedImage;
 
-    if (count <= 1)
-    {
+    if (count <= 1) {
         animatedImage = [[UIImage alloc] initWithData:data];
     }
-    else
-    {
+    else {
         NSMutableArray *images = [NSMutableArray array];
 
         NSTimeInterval duration = 0.0f;
 
-        for (size_t i = 0; i < count; i++)
-        {
+        for (size_t i = 0; i < count; i++) {
             CGImageRef image = CGImageSourceCreateImageAtIndex(source, i, NULL);
 
             duration += [self frameDurationAtIndex:i source:source];
@@ -45,9 +40,8 @@
             CGImageRelease(image);
         }
 
-        if (!duration)
-        {
-            duration = (1.0f/10.0f)*count;
+        if (!duration) {
+            duration = (1.0f / 10.0f) * count;
         }
 
         animatedImage = [UIImage animatedImageWithImages:images duration:duration];
@@ -58,20 +52,20 @@
     return animatedImage;
 }
 
-+ (float)frameDurationAtIndex:(NSUInteger)index source:(CGImageSourceRef)source
-{
++ (float)frameDurationAtIndex:(NSUInteger)index source:(CGImageSourceRef)source {
     float frameDuration = 0.1f;
-    CFDictionaryRef cfFrameProperties = CGImageSourceCopyPropertiesAtIndex(source,index,nil);
-    NSDictionary *frameProperties = (__bridge NSDictionary*)cfFrameProperties;
-    NSDictionary *gifProperties = frameProperties[(NSString*)kCGImagePropertyGIFDictionary];
+    CFDictionaryRef cfFrameProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil);
+    NSDictionary *frameProperties = (__bridge NSDictionary *)cfFrameProperties;
+    NSDictionary *gifProperties = frameProperties[(NSString *)kCGImagePropertyGIFDictionary];
 
-    NSNumber *delayTimeUnclampedProp = gifProperties[(NSString*)kCGImagePropertyGIFUnclampedDelayTime];
-    if(delayTimeUnclampedProp) {
+    NSNumber *delayTimeUnclampedProp = gifProperties[(NSString *)kCGImagePropertyGIFUnclampedDelayTime];
+    if (delayTimeUnclampedProp) {
         frameDuration = [delayTimeUnclampedProp floatValue];
-    } else {
+    }
+    else {
 
-        NSNumber *delayTimeProp = gifProperties[(NSString*)kCGImagePropertyGIFDelayTime];
-        if(delayTimeProp) {
+        NSNumber *delayTimeProp = gifProperties[(NSString *)kCGImagePropertyGIFDelayTime];
+        if (delayTimeProp) {
             frameDuration = [delayTimeProp floatValue];
         }
     }
@@ -81,93 +75,83 @@
     // a duration of <= 10 ms. See <rdar://problem/7689300> and <http://webkit.org/b/36082>
     // for more information.
 
-    if (frameDuration < 0.011f)
+    if (frameDuration < 0.011f) {
         frameDuration = 0.100f;
+    }
 
     CFRelease(cfFrameProperties);
     return frameDuration;
 }
 
-+ (UIImage *)sd_animatedGIFNamed:(NSString *)name
-{
++ (UIImage *)sd_animatedGIFNamed:(NSString *)name {
     CGFloat scale = [UIScreen mainScreen].scale;
-    
-    if (scale > 1.0f)
-    {
+
+    if (scale > 1.0f) {
         NSString *retinaPath = [[NSBundle mainBundle] pathForResource:[name stringByAppendingString:@"@2x"] ofType:@"gif"];
-        
+
         NSData *data = [NSData dataWithContentsOfFile:retinaPath];
-        
-        if (data)
-        {
+
+        if (data) {
             return [UIImage sd_animatedGIFWithData:data];
         }
-        
+
         NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
-        
+
         data = [NSData dataWithContentsOfFile:path];
-        
-        if (data)
-        {
+
+        if (data) {
             return [UIImage sd_animatedGIFWithData:data];
         }
-        
+
         return [UIImage imageNamed:name];
     }
-    else
-    {
+    else {
         NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
-        
+
         NSData *data = [NSData dataWithContentsOfFile:path];
-        
-        if (data)
-        {
+
+        if (data) {
             return [UIImage sd_animatedGIFWithData:data];
         }
-        
+
         return [UIImage imageNamed:name];
-    }    
+    }
 }
 
-- (UIImage *)sd_animatedImageByScalingAndCroppingToSize:(CGSize)size
-{
-    if (CGSizeEqualToSize(self.size, size) || CGSizeEqualToSize(size, CGSizeZero))
-    {
+- (UIImage *)sd_animatedImageByScalingAndCroppingToSize:(CGSize)size {
+    if (CGSizeEqualToSize(self.size, size) || CGSizeEqualToSize(size, CGSizeZero)) {
         return self;
     }
-    
+
     CGSize scaledSize = size;
     CGPoint thumbnailPoint = CGPointZero;
-    
+
     CGFloat widthFactor = size.width / self.size.width;
     CGFloat heightFactor = size.height / self.size.height;
-    CGFloat scaleFactor = (widthFactor > heightFactor) ? widthFactor :heightFactor;
+    CGFloat scaleFactor = (widthFactor > heightFactor) ? widthFactor : heightFactor;
     scaledSize.width = self.size.width * scaleFactor;
     scaledSize.height = self.size.height * scaleFactor;
-    
-    if (widthFactor > heightFactor)
-    {
-        thumbnailPoint.y = (size.height - scaledSize.height) * 0.5; 
+
+    if (widthFactor > heightFactor) {
+        thumbnailPoint.y = (size.height - scaledSize.height) * 0.5;
     }
-    else if (widthFactor < heightFactor)
-    {
+    else if (widthFactor < heightFactor) {
         thumbnailPoint.x = (size.width - scaledSize.width) * 0.5;
     }
-    
+
     NSMutableArray *scaledImages = [NSMutableArray array];
-    
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0); 
-    
-    for (UIImage *image in self.images)
-    {
+
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+
+    for (UIImage *image in self.images) {
         [image drawInRect:CGRectMake(thumbnailPoint.x, thumbnailPoint.y, scaledSize.width, scaledSize.height)];
         UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-        
+
         [scaledImages addObject:newImage];
     }
-    
+
     UIGraphicsEndImageContext();
-    
+
     return [UIImage animatedImageWithImages:scaledImages duration:self.duration];
 }
 
