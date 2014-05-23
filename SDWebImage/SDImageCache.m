d12d95222903f0e34d4e3c9388b1a2d0ee85ea8f
卢克 <luke.lkj@taobao.com>
2014-05-23 17:44:59 +0800

    It's generally a bad idea to remove items from a container while iterating through it. And fix the thread safe issue.

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -359,7 +359,7 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
 }
 
 - (void)cleanDiskWithCompletionBlock:(void (^)())completionBlock {
-    dispatch_async(self.ioQueue, ^{
+    dispatch_barrier_async(self.ioQueue, ^{
         NSURL *diskCacheURL = [NSURL fileURLWithPath:self.diskCachePath isDirectory:YES];
         NSArray *resourceKeys = @[NSURLIsDirectoryKey, NSURLContentModificationDateKey, NSURLTotalFileAllocatedSizeKey];
 
@@ -377,6 +377,7 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
         //
         //  1. Removing files that are older than the expiration date.
         //  2. Storing file attributes for the size-based cleanup pass.
+        NSMutableArray *urlsToDelete = [[NSMutableArray alloc] init];
         for (NSURL *fileURL in fileEnumerator) {
             NSDictionary *resourceValues = [fileURL resourceValuesForKeys:resourceKeys error:NULL];
 
@@ -388,7 +389,7 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
             // Remove files that are older than the expiration date;
             NSDate *modificationDate = resourceValues[NSURLContentModificationDateKey];
             if ([[modificationDate laterDate:expirationDate] isEqualToDate:expirationDate]) {
-                [_fileManager removeItemAtURL:fileURL error:nil];
+                [urlsToDelete addObject:fileURL];
                 continue;
             }
 
@@ -396,6 +397,10 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
             NSNumber *totalAllocatedSize = resourceValues[NSURLTotalFileAllocatedSizeKey];
             currentCacheSize += [totalAllocatedSize unsignedIntegerValue];
             [cacheFiles setObject:resourceValues forKey:fileURL];
+        }
+        
+        for (NSURL *fileURL in urlsToDelete) {
+            [_fileManager removeItemAtURL:fileURL error:nil];
         }
 
         // If our remaining disk cache exceeds a configured maximum size, perform a second
