6ad74ef182838fdc0beff5d42c9c90df2a07060d
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-06-15 18:08:32 +0300

    Exposed SDWebImageManager cacheKeyForURL

--- SDWebImage/SDWebImageManager.h
+++ SDWebImage/SDWebImageManager.h
@@ -219,4 +219,9 @@ SDWebImageManager *manager = [SDWebImageManager sharedManager];
 - (BOOL)cachedImageExistsForURL:(NSURL *)url;
 - (BOOL)diskImageExistsForURL:(NSURL *)url;
 
+/**
+ *Return the cache key for a given URL
+ */
+- (NSString *)cacheKeyForURL:(NSURL *)url;
+
 @end
